fb0c7139a3aec8693cb066ee7c25ffb7d10862bf
urielyan@sina.com <urielyan@sina.com>
2016-05-26 16:35:58 +0800

    修改设置时间（不用UI）实现

--- setupdatetime.h
+++ setupdatetime.h
@@ -2,8 +2,14 @@
 #define SETUPDATETIME_H
 
 #include <QWidget>
+#include <QButtonGroup>
+#include <QList>
+#include <QList>
 
+#include<QLabel>
+#include <QDateTime>
 namespace Ui {
+
 class setupdatetime;
 }
 
@@ -12,21 +18,42 @@ class setupdatetime : public QWidget
     Q_OBJECT
 
 public:
+    enum ENUM_DATETIME
+    {
+        YEAR = 0,
+        MONTH,
+        DAY,
+        HOUR,
+        MINUTE,
+    };
     explicit setupdatetime(QWidget *parent = 0);
-    ~setupdatetime();
   void show_and_refresh();
 
 private slots:
-    void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
 
-    void on_p_buttonPlus_clicked();
+    void on_p_buttonPlus_clicked();//useless
 
-    void on_p_buttonSub_clicked();
+    void on_p_buttonSub_clicked();//useless
+
+    void slotButtonUpClicked(int id);
+    void slotButtonDownClicked(int id);
 
 private:
     Ui::setupdatetime *ui;
+    QButtonGroup m_buttonGroupUp, m_buttonGroupDown;
+    QList<QLabel*> m_labelList;
+
+    void setLabelCurrentDateTime();
+    void examineDayLabelIsRight();
+    QString getSetDateTime();
 };
 
+class DateTimeFrame : public QFrame
+{
+    //useless
+    Q_OBJECT
+public:
+    DateTimeFrame(QWidget *parent = 0);
+};
 #endif // SETUPDATETIME_H
